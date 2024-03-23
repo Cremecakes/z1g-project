7bc01dcef427819f28b01d59301c86301ca945da
incognitotgt <tgt@incognitotgt.me>
2024-03-23 19:52:18 -0400

    favicon and eslint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,12 @@
-import { createPreset } from 'fumadocs-ui/tailwind-plugin';
-import { Config } from "tailwindcss"
+import { createPreset } from "fumadocs-ui/tailwind-plugin";
+import { Config } from "tailwindcss";
 const config: Config = {
   content: [
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './content/**/*.{md,mdx}',
-    './mdx-components.{ts,tsx}',
-    './node_modules/fumadocs-ui/dist/**/*.js',
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./content/**/*.{md,mdx}",
+    "./mdx-components.{ts,tsx}",
+    "./node_modules/fumadocs-ui/dist/**/*.js",
   ],
   presets: [createPreset()],
 };
