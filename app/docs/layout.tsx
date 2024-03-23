a3e9ccd205b5af406dff6fbdb9e723effc15b1e3
incognitotgt <tgt@incognitotgt.me>
2024-03-23 19:24:32 -0400

    fix a funny

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,34 +1,7 @@
-import { pageTree } from "../source";
-import { DocsLayout, type DocsLayoutProps } from "fumadocs-ui/layout";
-import { Discord } from "@/components/icons";
-import Image from "next/image";
+import { DocsLayout } from "fumadocs-ui/layout";
+import { layoutOptions } from "../layout-options";
 import type { ReactNode } from "react";
-export const layoutOptions: Omit<DocsLayoutProps, "children"> = {
-  tree: pageTree,
-  nav: {
-    title: (
-      <>
-        <Image
-          src="/logo.png"
-          alt="z1g Project"
-          width={32}
-          height={32}
-          className="max-md:hidden"
-        />
-        <span className="ml-3 font-semibold max-md:hidden">z1g Project</span>
-      </>
-    ),
-    transparentMode: "top",
-    links: [
-      {
-        label: "Discord",
-        href: "https://discord.gg/6cpcbKwjBn",
-        icon: <Discord className="h-6 w-6" />,
-      },
-    ],
-    githubUrl: "https://github.com/z1g-project",
-  },
-};
+
 export default function RootDocsLayout({ children }: { children: ReactNode }) {
   return <DocsLayout {...layoutOptions}>{children}</DocsLayout>;
 }
---
+++ app/layout-options.tsx
@@ -0,0 +1,30 @@
+import { pageTree } from "./source";
+import { type DocsLayoutProps } from "fumadocs-ui/layout";
+import { Discord } from "@/components/icons";
+import Image from "next/image";
+export const layoutOptions: Omit<DocsLayoutProps, "children"> = {
+  tree: pageTree,
+  nav: {
+    title: (
+      <>
+        <Image
+          src="/logo.png"
+          alt="z1g Project"
+          width={32}
+          height={32}
+          className="max-md:hidden"
+        />
+        <span className="ml-3 font-semibold max-md:hidden">z1g Project</span>
+      </>
+    ),
+    transparentMode: "top",
+    links: [
+      {
+        label: "Discord",
+        href: "https://discord.gg/6cpcbKwjBn",
+        icon: <Discord className="h-6 w-6" />,
+      },
+    ],
+    githubUrl: "https://github.com/z1g-project",
+  },
+};
