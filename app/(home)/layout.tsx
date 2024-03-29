359000404776015c826b02cbe36cd49172ebfa2f
incognitotgt <tgt@incognitotgt.me>
2024-03-28 20:22:55 -0400

    navbar

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,5 +1,6 @@
 import { Layout } from "fumadocs-ui/layout";
 import Link from "next/link";
+import Image from "next/image";
 import { getServerSession } from "next-auth";
 import authConfig from "@/auth.config";
 import type { ReactNode } from "react";
@@ -19,14 +20,37 @@ export default async function HomeLayout({
           ...layoutOptions.nav,
           children: (
             <>
+              {session && (
+                <>
+                  <div className="text-secondary-foreground text-sm flex justify-center items-center">
+                    <Image
+                      src={session?.user?.image as string}
+                      alt="User Image"
+                      width={25}
+                      height={25}
+                      className="rounded-full mr-2"
+                    />
+                    Welcome, {session?.user?.name || session?.user?.email}
+                  </div>
+                  <div className="text-foreground">|</div>
+                </>
+              )}
               {layoutOptions.nav && layoutOptions.nav.children}
               {session ? (
-                <Link
-                  href="/auth/logout"
-                  className="hover:text-foreground text-muted-foreground text-sm duration-300"
-                >
-                  Sign Out
-                </Link>
+                <>
+                  <Link
+                    href="/admin"
+                    className="hover:text-foreground text-muted-foreground text-sm duration-300"
+                  >
+                    Admin
+                  </Link>
+                  <Link
+                    href="/auth/logout"
+                    className="hover:text-foreground text-muted-foreground text-sm duration-300"
+                  >
+                    Sign Out
+                  </Link>
+                </>
               ) : (
                 <Link
                   href="/auth/login"
