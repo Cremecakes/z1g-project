a57cdf8d1046964b5afc29303f8fabe8e2fd9033
incognitotgt <tgt@incognitotgt.me>
2024-03-31 01:58:18 -0400

    mistake

--- app/(home)/admin/layout.tsx
+++ app/(home)/admin/layout.tsx
@@ -1,7 +1,12 @@
+import type { ReactNode } from "react";
 import { auth } from "@/auth.config";
 import { redirect } from "next/navigation";
 
-export default async function AdminLayout({ children }: React.ReactNode) {
+export default async function AdminLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
   const session = await auth();
   if (!session) {
     return redirect("/auth/login");
